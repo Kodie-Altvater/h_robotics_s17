157ff3dbea7f773f9ce6ad9496294f9b348d84ce
kaltvat1 <kaltvat1@jhu.edu>
2017-04-20 23:37:38 -0400

    Modified socket_tcp.py to now allow for arbitrarily sized data. Next step is to pass data from matlab capture image into python

--- image_training.py
+++ image_training.py
@@ -1,5 +1,6 @@
 # import the necessary modules
 import freenect
+from socket_tcp import *
 import cv2
 import time
 import numpy as np
@@ -20,53 +21,48 @@ def get_depth():
     return array
 
 
-if __name__ == "__main__":
+# function to classify image and spit image back
+def get_image(img_data):
+    # Set path to classifiers found in OpenCV install directory
+    path = normpath(realpath(cv2.__file__) + '../../../../../share/OpenCV/haarcascades')
+
+    # get a frame from RGB camera
+    img = get_video()
+
+    # get a frame from depth sensor
+    #depth = get_depth()
+
+    # These are the haargrave classifiers that are needed for image detection
+    face_cascade = cv2.CascadeClassifier(path + '/haarcascade_frontalface_default.xml')
+    eye_cascade  = cv2.CascadeClassifier(path + '/haarcascade_eye.xml')
+
+    # Convert image to grayscale
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    # detects face location
+    faces = face_cascade.detectMultiScale(gray, 1.2, 5)
+
+    for (x, y, w, h) in faces:
+        cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
+        roi_gray  = gray[y:y + h, x:x + w]
+        roi_color = img[y:y + h, x:x + w]
+        eyes = eye_cascade.detectMultiScale(roi_gray)
+        for (ex, ey, ew, eh) in eyes:
+            cv2.rectangle(roi_color, (ex, ey), (ex + ew, ey + eh), (0, 255, 0), 2)
+
+    # continously outputs image to screen for viewing
+    cv2.imshow('img',img)
+    k = cv2.waitKey(5) & 0xFF
+
+    time.sleep(.1)
+    cv2.destroyAllWindows()
+
+    return img
+
+
+if __name__ == '__main__':
+
     while 1:
-
-        # Set path to classifiers found in OpenCV install directory
-        path = normpath(realpath(cv2.__file__) + '../../../../../share/OpenCV/haarcascades')
-
-        img = cv2.imread('/Users/Schmoder/Desktop/human1.jpg')
-        #cv2.imshow('img', img)
-
-        # get a frame from RGB camera
-        #img = get_video()
-
-        # get a frame from depth sensor
-        #depth = get_depth()
-
-        face_cascade = cv2.CascadeClassifier(path + '/haarcascade_frontalface_default.xml')
-        eye_cascade  = cv2.CascadeClassifier(path + '/haarcascade_eye.xml')
-
-        # Convert image to grayscale
-        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
-        #cv2.imshow('img', gray)
-
-        faces = face_cascade.detectMultiScale(gray, 1.2, 5)
-
-
-        for (x, y, w, h) in faces:
-            cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
-            roi_gray  = gray[y:y + h, x:x + w]
-            roi_color = img[y:y + h, x:x + w]
-            eyes = eye_cascade.detectMultiScale(roi_gray)
-            for (ex, ey, ew, eh) in eyes:
-                cv2.rectangle(roi_color, (ex, ey), (ex + ew, ey + eh), (0, 255, 0), 2)
-        cv2.imshow('img',img)
-        print 'hey'
-
-
-        # display RGB image
-        #cv2.imshow('RGB image', video)
-
-        # display depth image
-        #cv2.imshow('Depth image', depth)
-
-        time.sleep(.1)
-
-        # quit program when 'esc' key is pressed
-        k = cv2.waitKey(5) & 0xFF
-        if k == 27:
-            break
-    cv2.destroyAllWindows( )
+        data = tcp_data('127.0.0.1',50000)
+        print data
+        #get_image()
--- socket_tcp.py
+++ socket_tcp.py
@@ -1,18 +1,44 @@
+
 import socket
 
-TCP_IP = '127.0.0.1'
-TCP_PORT = 50007
-BUFFER_SIZE = 20  # Normally 1024, but we want fast response
+def tcp_data(ip_addr,sock):
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((TCP_IP, TCP_PORT))
-s.listen(1)
+    TCP_IP = ip_addr
+    TCP_PORT = sock
+    BUFFER_SIZE = 20  # Normally 1024, but we want fast response
 
-conn, addr = s.accept()
-print 'Connection address:', addr
-while 1:
-    data = conn.recv(BUFFER_SIZE)
-    if not data: break
-    print "received data:", data
-    conn.send(data)  # echo
-conn.close()
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind((TCP_IP, TCP_PORT))
+    s.listen(1)
+
+    conn, addr = s.accept()
+    print 'Connection address:', addr
+
+    while 1:
+        data = ''
+        while len(data) < 2:
+            rcv = conn.recv(2 - len(data))
+            data = data + rcv
+            if rcv == '':
+                break
+        if rcv == '':
+            break
+        frame_len = (ord(data[0])<<8) + ord(data[1])
+
+
+        data = ''
+        while len(data) < frame_len:
+            rcv = conn.recv(frame_len - len(data))
+            data = data + rcv
+            if rcv == '':
+                break
+        if rcv == '':
+            break
+
+        tmpdata = []
+        for i in data:
+            tmpdata.append(ord(i))
+        print "received data:", tmpdata
+        conn.send(data)  # echo
+    conn.close()
+    return data
